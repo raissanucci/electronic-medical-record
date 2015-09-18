d17f6e6d3fc3068108bbb6b0fca78abbab3d9881
Fellipe Caetano <fellipe.caetano4@gmail.com>
2015-09-18 13:30:22 -0300

    Project generation based on Thucydides Maven archetype
    The specification of the project determines that we use a predetermined behavior-driven development stack, consisting of JBehave as a the test driver, Selenium for functional tests and Thucydides for test reporting and analytics. I used a Maven archetype to generate a project based on this configuration.

---
+++ src/test/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/jbehave/AcceptanceTestSuite.java
@@ -0,0 +1,5 @@
+package br.unicamp.mc437.grupo3.electronicmedicalrecord.jbehave;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class AcceptanceTestSuite extends ThucydidesJUnitStories {}
