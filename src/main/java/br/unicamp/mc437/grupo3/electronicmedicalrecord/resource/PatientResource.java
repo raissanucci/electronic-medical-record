d994f0bb737f5181e0825d0330ac924f81e8b4ae
Fellipe Caetano <fellipe.caetano4@gmail.com>
2015-09-19 17:54:02 -0300

    Creating a PatientResource to receive HTTP requests regarding the persistence of Patient objects

--- src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/resource/PatientResource.java
+++ src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/resource/PatientResource.java
@@ -1,16 +1,42 @@
 package br.unicamp.mc437.grupo3.electronicmedicalrecord.resource;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import br.unicamp.mc437.grupo3.electronicmedicalrecord.model.Patient;
+import br.unicamp.mc437.grupo3.electronicmedicalrecord.persistence.ServletContextPersistenceWrapper;
+import br.unicamp.mc437.grupo3.electronicmedicalrecord.persistence.repository.PatientRepository;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/patient")
 public class PatientResource {
+    @Context
+    private ServletContext servletContext;
+
     @GET
-    @Path("/name")
+    @Path("/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getName() {
-        return "Fellipe Caetano";
+    public String get(@PathParam("id") Integer patientId) {
+        ServletContextPersistenceWrapper servletContextPersistenceWrapper = new ServletContextPersistenceWrapper(servletContext);
+        // TODO: There should typically be only one EntityManager per application. This is totally and completely wrong and should be temporary.
+        EntityManager entityManager = servletContextPersistenceWrapper.getEntityManagerFactory().createEntityManager();
+        PatientRepository patientRepository = new PatientRepository(entityManager);
+        Patient patient = patientRepository.get(patientId);
+        return patient.getName();
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public String post(@FormParam("name") String patientName) {
+        ServletContextPersistenceWrapper servletContextPersistenceWrapper = new ServletContextPersistenceWrapper(servletContext);
+        // TODO: There should typically be only one EntityManager per application. This is totally and completely wrong and should be temporary.
+        EntityManager entityManager = servletContextPersistenceWrapper.getEntityManagerFactory().createEntityManager();
+        PatientRepository patientRepository = new PatientRepository(entityManager);
+        Patient patient = new Patient(patientName);
+        patientRepository.add(patient);
+        return patientName;
     }
 }
