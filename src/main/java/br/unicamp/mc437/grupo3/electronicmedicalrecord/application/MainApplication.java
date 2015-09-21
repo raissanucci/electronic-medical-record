ae4a8c145414816135cf83a68904e6b78a950b50
Fellipe Caetano <fellipe.caetano4@gmail.com>
2015-09-21 01:30:10 -0300

    Creating an JAX-RS Application subclass that defines an application path for the API

---
+++ src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/application/MainApplication.java
@@ -0,0 +1,7 @@
+package br.unicamp.mc437.grupo3.electronicmedicalrecord.application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MainApplication extends Application {}
