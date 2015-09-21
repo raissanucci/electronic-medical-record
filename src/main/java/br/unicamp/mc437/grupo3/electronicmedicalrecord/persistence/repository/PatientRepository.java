6d58a756b320bc425fa463319850960c31c73abf
Fellipe Caetano <fellipe.caetano4@gmail.com>
2015-09-21 00:14:49 -0300

    Changing our pom.xml: application is now packed as a war file
    We need to pack our app as war file so it can be deployed to any Java-compatible servers. I need to tweak our dependencies a bit so that TomEE can start with our app

--- src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/persistence/repository/PatientRepository.java
+++ src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/persistence/repository/PatientRepository.java
@@ -2,25 +2,21 @@ package br.unicamp.mc437.grupo3.electronicmedicalrecord.persistence.repository;
 
 import br.unicamp.mc437.grupo3.electronicmedicalrecord.model.Patient;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+@Stateless
 public class PatientRepository {
+    @PersistenceContext(unitName = "development")
     private EntityManager entityManager;
 
-    public PatientRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
     public void add(Patient patient) {
-        entityManager.getTransaction().begin();
         entityManager.persist(patient);
-        entityManager.getTransaction().commit();
     }
 
     public Patient get(Integer patientId) {
-        entityManager.getTransaction().begin();
         Patient patient = entityManager.find(Patient.class, patientId);
-        entityManager.getTransaction().commit();
         return patient;
     }
 }
--- src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/resource/PatientResource.java
+++ src/main/java/br/unicamp/mc437/grupo3/electronicmedicalrecord/resource/PatientResource.java
@@ -1,28 +1,18 @@
 package br.unicamp.mc437.grupo3.electronicmedicalrecord.resource;
 
 import br.unicamp.mc437.grupo3.electronicmedicalrecord.model.Patient;
-import br.unicamp.mc437.grupo3.electronicmedicalrecord.persistence.ServletContextPersistenceWrapper;
 import br.unicamp.mc437.grupo3.electronicmedicalrecord.persistence.repository.PatientRepository;
 
-import javax.persistence.EntityManager;
-import javax.servlet.ServletContext;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/patient")
 public class PatientResource {
-    @Context
-    private ServletContext servletContext;
-
     @GET
     @Path("/{id}")
     @Produces(MediaType.TEXT_PLAIN)
     public String get(@PathParam("id") Integer patientId) {
-        ServletContextPersistenceWrapper servletContextPersistenceWrapper = new ServletContextPersistenceWrapper(servletContext);
-        // TODO: There should typically be only one EntityManager per application. This is totally and completely wrong and should be temporary.
-        EntityManager entityManager = servletContextPersistenceWrapper.getEntityManagerFactory().createEntityManager();
-        PatientRepository patientRepository = new PatientRepository(entityManager);
+        PatientRepository patientRepository = new PatientRepository();
         Patient patient = patientRepository.get(patientId);
         return patient.getName();
     }
@@ -31,10 +21,7 @@ public class PatientResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public String post(@FormParam("name") String patientName) {
-        ServletContextPersistenceWrapper servletContextPersistenceWrapper = new ServletContextPersistenceWrapper(servletContext);
-        // TODO: There should typically be only one EntityManager per application. This is totally and completely wrong and should be temporary.
-        EntityManager entityManager = servletContextPersistenceWrapper.getEntityManagerFactory().createEntityManager();
-        PatientRepository patientRepository = new PatientRepository(entityManager);
+        PatientRepository patientRepository = new PatientRepository();
         Patient patient = new Patient(patientName);
         patientRepository.add(patient);
         return patientName;
