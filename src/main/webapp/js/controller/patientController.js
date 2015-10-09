519c4de553545bd9d9a6901c7e548d9909f2e195
André Terron <andrev.terron@gmail.com>
2015-10-09 20:25:17 -0300

    Adding the screen to create new patient

--- src/main/webapp/js/controller/patientController.js
+++ src/main/webapp/js/controller/patientController.js
@@ -1,3 +1,27 @@
 ngapp.controller('patientListController', [function($scope) {
-    
+    $scope.patient = {
+        name: "",
+        socialName: "",
+        mothersName: "",
+        fathersName: "",
+        gender: "",
+        color: "",
+        birthdate: "",
+        bloodtype: "",
+        email: "",
+        cpf: "",
+        rg: "",
+        orgEmissor: "",
+        orgEmissorUF: "",
+        ddd: "",
+        phone: "",
+        zip: "",
+        address: "",
+        addressNumber: "",
+        addressComplement: "",
+        addressCityArea: "",
+        addressCity: "",
+        addressUF: "",
+        addressCountry: ""
+    };
 }])
