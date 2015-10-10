7d96b8d70a3e9f49e510b641c28a6c5c8cbb6ca0
André Terron <andrev.terron@gmail.com>
2015-10-09 23:04:04 -0300

    lalala

--- src/main/webapp/js/api/patient-api.js
+++ src/main/webapp/js/api/patient-api.js
@@ -5,28 +5,28 @@ angular.module('electronic-medical-record-api')
 
 .factory('PatientApi', ['Api', function(Api) {
   return {
-    
+
     /**
      * Get the patient list
      */
     patients: function(query) {
       return Api.get('/patients', null, query);
     },
-    
+
     /**
      * Creates a new patient
      */
     newPatient: function(entity) {
-      return Api.post('/patients', entity);
+      return Api.post('/patients', entity, {'Content-Type': 'application/x-www-form-urlencoded'});
     },
-    
+
     /**
      * Get data about the patient
      */
     getPatient: function(id) {
       return Api.get('/patients', id);
     },
-    
+
     /**
      * Updates the data about the patient
      */
